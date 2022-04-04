776451683961091ad4262d41d5b0811dd16caf33
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-04 14:40:41 -0300

    feat: ✨  adds form input fields

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 
 export function Form() {
   return (
-    <h1>Hello there!</h1>
+    <>
+      <form>
+        <label>Nome</label>
+        <input type="text" />
+        
+        <label>Sobrenome</label>
+        <input type="text" />
+        
+        <label>CPF</label>
+        <input type="text" />
+        
+        <label>Promoções</label>
+        <input type="checkbox" />
+      </form>
+    </>
   )
 }
