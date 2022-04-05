82ad881b25f24f36ddb7e45122ca40cef598ec72
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-05 08:44:28 -0300

    feat: ✨ adjust app width

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Form } from './components/form/Form'
 
+import { Container } from '@mui/material'
+
 function App() {
   return (
-    <>
+    <Container component="article" maxWidth="sm">
       <Form />
-    </>
+    </Container>
   )
 }
 
