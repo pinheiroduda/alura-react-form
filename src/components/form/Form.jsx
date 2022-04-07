a12a6858ff4b6a5688b2c43df3496fee476c9889
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-07 13:32:41 -0300

    feat: ✨ adds state to name and lastname form fields

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import {
   Typography,
@@ -11,14 +11,44 @@ import {
 import '@fontsource/roboto/400.css'
 
 export function Form() {
+  const [name, setName] = useState('')
+  const [lastname, setLastname] = useState('')
+
   return (
     <>
       <Typography variant="h3" maxWidth="sm">
         Formulário de cadastro
       </Typography>
-      <form>
-        <TextField id="name" label="Nome" fullWidth margin="normal" />
-        <TextField id="lastname" label="Sobrenome" fullWidth margin="normal" />
+      <form
+        onSubmit={event => {
+          event.preventDefault()
+          console.log(name, lastname)
+        }}
+      >
+        <TextField
+          value={name}
+          onChange={event => {
+            let temporaryName = event.target.value
+            if (temporaryName.length > 4) {
+              temporaryName = temporaryName(name.substring(0, 3))
+            }
+            setName(temporaryName)
+          }}
+          id="name"
+          label="Nome"
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          value={lastname}
+          onChange={event => {
+            setLastname(event.target.value)
+          }}
+          id="lastname"
+          label="Sobrenome"
+          fullWidth
+          margin="normal"
+        />
         <TextField id="cpf" label="CPF" fullWidth margin="normal" />
         <FormControlLabel
           label="Promoções"
