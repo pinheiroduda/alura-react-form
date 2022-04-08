5c57a79b91ef20d06817a31212df7ecdbf4020c4
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-08 11:22:53 -0300

    feat: ✨ send data just when all form data are collected

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,18 +4,23 @@ import { DeliveryData } from './DeliveryData'
 import { PersonalData } from './PersonalData'
 import { UserData } from './UserData'
 
+import { Typography } from '@mui/material'
+
 export function Form({ onSubmit, validateCPF }) {
   const [currentStage, setCurrentStage] = useState(0)
   const [collectedData, setCollectedData] = useState({})
 
   useEffect(() => {
-    console.log(collectedData)
+    if (currentStage === forms.length - 1) {
+      onSubmit(collectedData)
+    }
   })
 
   const forms = [
     <UserData onSubmit={getData} />,
     <PersonalData onSubmit={getData} validateCPF={validateCPF} />,
-    <DeliveryData onSubmit={getData} />
+    <DeliveryData onSubmit={getData} />,
+    <Typography variant="h5">Obrigado por se cadastrar!</Typography>
   ]
 
   function getData(data) {
