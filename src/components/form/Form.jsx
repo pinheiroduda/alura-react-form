644da6bb00d035a6299b55e8b042606cde6f3f70
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-08 08:54:18 -0300

    feat: ✨ creates switch case to render forms conditionally

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import { DeliveryData } from './DeliveryData'
+import React, { useState } from 'react'
 
+import { Typography } from '@mui/material'
+
+import { DeliveryData } from './DeliveryData'
 import { PersonalData } from './PersonalData'
 import { UserData } from './UserData'
 
 export function Form({ onSubmit, validateCPF }) {
-  return (
-    <>
-      <PersonalData onSubmit={onSubmit} validateCPF={validateCPF} />
-      <UserData />
-      <DeliveryData />
-    </>
-  )
+  const [currentStage, setCurrentStage] = useState(0)
+  return <>{CurrentForm(currentStage)}</>
+}
+
+function CurrentForm(stage) {
+  switch (stage) {
+    case 0:
+      return <UserData />
+    case 1:
+      return <PersonalData />
+    case 2:
+      return <DeliveryData />
+    default:
+      return <Typography>Erro ao selecionar formilário</Typography>
+  }
 }
