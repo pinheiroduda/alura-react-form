ca739d4c36c8584b43f8594505ea7b69fa2770da
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-08 08:27:44 -0300

    feat: ✨ adds text fields and button to user data component

--- src/components/form/UserData.jsx
+++ src/components/form/UserData.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import { TextField } from '@mui/material'
+import { TextField, Button } from '@mui/material'
 
 export function UserData() {
   return (
     <>
       <form>
-        <TextField id="email" label="email" type="email"></TextField>
+        <TextField
+          id="email"
+          label="email"
+          type="email"
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          id="password"
+          label="senha"
+          type="password"
+          fullWidth
+          margin="normal"
+        />
+        <Button type="submit" variant="contained">
+          Cadastrar
+        </Button>
       </form>
     </>
   )
