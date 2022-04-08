93399f94405cb985db90ea171e4fefbb39a99e25
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-08 08:30:21 -0300

    fix: 🐛 validation of cpf field

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Container } from '@mui/material'
 function App() {
   return (
     <Container component="article" maxWidth="sm">
-      <Form />
+      <Form onSubmit={onSubmitForm} validateCPF={validateCPF} />
     </Container>
   )
 }
