dfb16ceb9c9869568a1c037e43d56836a4f4a87b
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-08 11:12:04 -0300

    feat: ✨ get data insert by user

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { DeliveryData } from './DeliveryData'
 import { PersonalData } from './PersonalData'
@@ -6,12 +6,22 @@ import { UserData } from './UserData'
 
 export function Form({ onSubmit, validateCPF }) {
   const [currentStage, setCurrentStage] = useState(0)
+  const [collectedData, setCollectedData] = useState({})
+
+  useEffect(() => {
+    console.log(collectedData)
+  })
 
   const forms = [
-    <UserData onSubmit={nextStage} />,
-    <PersonalData onSubmit={nextStage} validateCPF={validateCPF} />,
-    <DeliveryData />
+    <UserData onSubmit={getData} />,
+    <PersonalData onSubmit={getData} validateCPF={validateCPF} />,
+    <DeliveryData onSubmit={getData} />
   ]
+
+  function getData(data) {
+    setCollectedData({ ...collectedData, ...data })
+    nextStage()
+  }
 
   function nextStage() {
     setCurrentStage(currentStage + 1)
