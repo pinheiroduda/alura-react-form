ebb186b3c7f8d4d36fe8a4d6d5bb4113628a4f2d
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-08 09:57:54 -0300

    refactor: ♻️ improves forms conditonal rendering readability

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
 
-import { Typography } from '@mui/material'
-
 import { DeliveryData } from './DeliveryData'
 import { PersonalData } from './PersonalData'
 import { UserData } from './UserData'
@@ -9,22 +7,15 @@ import { UserData } from './UserData'
 export function Form({ onSubmit, validateCPF }) {
   const [currentStage, setCurrentStage] = useState(0)
 
+  const forms = [
+    <UserData onSubmit={nextStage} />,
+    <PersonalData onSubmit={nextStage} validateCPF={validateCPF} />,
+    <DeliveryData />
+  ]
+
   function nextStage() {
     setCurrentStage(currentStage + 1)
   }
 
-  function CurrentForm(stage) {
-    switch (stage) {
-      case 0:
-        return <UserData onSubmit={nextStage} />
-      case 1:
-        return <PersonalData onSubmit={nextStage} validateCPF={validateCPF} />
-      case 2:
-        return <DeliveryData />
-      default:
-        return <Typography>Erro ao selecionar formilário</Typography>
-    }
-  }
-
-  return <>{CurrentForm(currentStage)}</>
+  return <>{forms[currentStage]}</>
 }
